3fa64f017c4a1ac6eb2fa0dbdd090a5899de5806
EmmanuelGc241 <92220547+EmmanuelGc241@users.noreply.github.com>
2021-10-17 22:55:01 -0500

    Terminando detalles en del

--- main.js
+++ main.js
@@ -17,10 +17,15 @@ function deleteAll() {
   result = dataNum + borrar;
   document.getElementsByClassName("resultado")[0].innerHTML = result;
 }
-
+//
 function del() {
   var number = result;
   var delate = number.substring(0, number.length - 1);
   document.getElementsByClassName("resultado")[0].innerHTML = delate;
   result = result.substring(0, result.length - 1);
+
+  if (result == 0) {
+    result = "0";
+    document.getElementsByClassName("resultado")[0].innerHTML = result;
+  }
 }
